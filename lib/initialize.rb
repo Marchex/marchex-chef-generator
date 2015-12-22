c302349916773ba9ce32230057935e3cc648dcd7
Tony Flint <tflint@marchex.com>
2015-12-22 10:07:05 -0800

    Switch to using Dir.chdir to run git pull inside repo directory rather than relying on git -C option which doesn't exist in the 12.04 apt-get version of git.

--- lib/initialize.rb
+++ lib/initialize.rb
@@ -1,11 +1,13 @@
 puts "Initializing custom Marchex chef generator..."
 generator_dir = File.dirname(__FILE__)
-update_repo_command = "git -C #{generator_dir} pull --rebase 2>&1"
-update_repo_command_output = `#{update_repo_command}`
-unless ($?.success?)
-  raise     "'#{update_repo_command}' failed with exit code #{$?.exitstatus} and output:\n\n#{update_repo_command_output}\n" \
-        <<  "Please fix this error and re-run the command."
-end
+Dir.chdir(generator_dir) {
+  update_repo_command = "git pull --rebase 2>&1"
+  update_repo_command_output = `#{update_repo_command}`
+  unless ($?.success?)
+    raise     "'#{update_repo_command}' failed with exit code #{$?.exitstatus} and output:\n\n#{update_repo_command_output}\n" \
+          <<  "Please fix this error and re-run the command."
+  end
+}
 
 require "#{generator_dir}/launch_screen.rb"
 
