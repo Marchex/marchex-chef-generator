e483700db50b9bcb5570cfac124beaee5e870177
Ivan Zbinden <izbinden@marchex.com>
2016-06-02 12:33:30 -0700

    Initial commit.

---
+++ izbinden_test_cookbook/metadata.rb
@@ -0,0 +1,10 @@
+name 'izbinden_test_cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures izbinden_test_cookbook'
+long_description 'Installs/Configures izbinden_test_cookbook'
+version '0.1.0'
+
+issues_url 'https://jira.marchex.com'
+source_url 'https://github.marchex.com/marchex-chef/izbinden_test_cookbook'
---
+++ izbinden_test_cookbook/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: izbinden_test_cookbook
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
