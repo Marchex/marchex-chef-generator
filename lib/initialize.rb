db93a87d4851ce574e6fb18117141fd10f0a9388
Tony Flint <tflint@marchex.com>
2016-06-28 09:40:22 -0700

    Don't check for latest code until we find a better way.

--- lib/initialize.rb
+++ lib/initialize.rb
@@ -2,14 +2,16 @@ require 'chef'
 require 'chef/knife'
 
 # Find the latest sha of the generator code and complain if the local copy is out of date
-generator_dir = "#{File.dirname(__FILE__)}/.."
-Dir.chdir(generator_dir) {
-  current_sha = `git rev-parse @`
-  remote_sha  = `git rev-parse @{u}`
-  unless (current_sha == remote_sha)
-    Chef::Log.warn("### WARNING ### You do not have the latest code checked out, please run 'cd #{generator_dir}' and do a 'git pull'")
-  end
-}
+# Comment out until we find a better way to determine whether you have the latest code - 
+# git rev-parse @ is only in newer version of git not available in the standard 12.04 apt repo
+# generator_dir = "#{File.dirname(__FILE__)}/.."
+# Dir.chdir(generator_dir) {
+  # current_sha = `git rev-parse @`
+  # remote_sha  = `git rev-parse @{u}`
+  # unless (current_sha == remote_sha)
+    # Chef::Log.warn("### WARNING ### You do not have the latest code checked out, please run 'cd #{generator_dir}' and do a 'git pull'")
+  # end
+# }
 
 # Always run vault commands in client mode
 Chef::Config[:knife][:vault_mode] = 'client'
